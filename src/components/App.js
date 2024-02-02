4f87209db4073ede9ee7371759b6a7bb7f5b8cfd
Priscilla-22 <priscillamw123@gmail.com>
2024-02-02 18:53:11 +0300

    feat: fetch dogs random images from an API and set state

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React, { useEffect, useState } from 'react';
+function App() {
+  const [dogImgUrl, setDogImgUrl] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((r) => r.json())
+      .then((data) => {
+        setDogImgUrl(data.message);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setIsLoading(false);
+      });
+  },[]);
+
+  if (isLoading) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <img src={dogImgUrl} alt='A Random Dog' />
+    </div>
+  );
+}
+export default App;
